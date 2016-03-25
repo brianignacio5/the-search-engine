7bd9f9b95a2039c22a4704655c8cb6a67a6644eb
Moritz Schäfer <mail@moritzs.de>
2016-03-25 20:01:19 +0800

    project organization

---
+++ setup.py
@@ -0,0 +1,18 @@
+from setuptools import find_packages, setup
+
+EXCLUDE_FROM_PACKAGES = []
+
+setup(
+    name='tsg',
+    version=0.01,
+    #  url='',
+    author='Moritz, Miguel and Brian',
+    author_email='mail@moritzs.de',
+    description=('A cool search engine and \
+                 crawler for http://dblp.uni-trier.de/'),
+    license='',
+    packages=find_packages(exclude=EXCLUDE_FROM_PACKAGES),
+    include_package_data=True,
+    scripts=[],
+    requires=['requests', 'nose']
+)
