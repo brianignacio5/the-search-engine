e2504c466cd144f6128fa1c50c89bcc190f1f4b1
Brian Ignacio <brianignacio5@gmail.com>
2016-04-06 04:00:45 +0800

    Define functions in tsg ranker base

--- test/test_ranker.py
+++ test/test_ranker.py
@@ -1,7 +1,7 @@
 import math
 
 from tsg.config import DICTIONARY_PATH
-from tsg.ranker import get_dictionary_term_list, Cosine_score_calc
+from tsg.ranker import get_dictionary_term_list, cosine_score_calc, calculate_query_term_weight
 
 def test_extract_termfile() :
 
@@ -13,7 +13,7 @@ def test_extract_termfile() :
 	with open(DICTIONARY_PATH) as dict_f:
 		for line in dict_f:
 			if term in line:
-				parts = line.split(term).split(',')
+				parts = line[len(term)+1:].split(term).split(',')
 				for doc_data in parts:
 					doc_data_parts = parts.split(':')
 					term_list[doc_data_parts[0]] = doc_data_parts[1]
@@ -27,26 +27,26 @@ def test_cosine_score_calc() :
 	query = 'Query to search'
 	parts = query.split(' ')
 	term1 = parts[0]
-	term1_list = get_dictionary_term_list(term)
+	term1_list = get_dictionary_term_list(term1)
 
 	term2 = parts[1]
-	term2_list = get_dictionary_term_list(term)
+	term2_list = get_dictionary_term_list(term2)
 	Score = [] # Holds tf-idf value for each doc- query term pair
 	Length = [] # Holds numbers of docs for each term
 
-	for term in query_terms:
+	for term in parts:
 		query_term_weight = calculate_query_term_weight(term,query)
-		term_list = get_dictionary_term_list(term)
-		for doc in term_list:
-			Score[doc_Id] += get_tf_idf_score(doc)*query_term_weight
-			Length[doc_Id] += math.pow(Score[doc_Id],2)
+		term_list = [term1_list,term2_list]
+		for key in term_list:
+			Score[key] += term_list[key]*query_term_weight
+			Length[key] += math.pow(Score[key],2)
 
 	for i, score in enumerate(Score):
 		Score[i] = score / Length[i]
 	
-	Docs_assert = get_docs_by_score(Score)
+	Docs_assert = cosine_score_calc(Score)
 
-	Docs_results = Cosine_score_calc(query)
+	Docs_results = cosine_score_calc(query)
 
 	assert Docs_results[:2] == Docs_assert[:2]
 
--- tsg/ranker/base.py
+++ tsg/ranker/base.py
@@ -1,9 +1,47 @@
+import math
+import os, os.path
+import numpy as np
+from tsg.config import DICTIONARY_PATH, RAW_DIR
 
+def get_dictionary_term_list(term) :
+	term_list = []
+	with open(DICTIONARY_PATH) as dict_f:
+		for line in dict_f:
+			if term in line:
+				parts = line[len(term)+1:].split(term).split(',')
+				term_list = []
+				for doc_data in parts:
+					doc_data_parts = parts.split(':')
+					term_list[doc_data_parts[0]] = doc_data_parts[1]
+	return term_list
 
-def Dictionary_term_list() :
-	term_lists = {}
-	return term_lists
+def calculate_query_term_weight(term,query):
+	term_dictionary = get_dictionary_term_list(term)
+	N = len([name for name in os.listdir(RAW_DIR) if os.path.isfile(name)])
+	doc_freq = len(term_dictionary)
+	term_freq = query.lower().count(term)
+	weight = (1+ np.log10(term_freq))*math.log10(N/doc_freq)
 
-def Cosine_score_calc(): 
+	return weight
+
+def cosine_score_calc(query): 
+	term_lists = { }
+	query_terms = query.split('')
+	for term in query_terms:
+		term_list = get_dictionary_term_list(term)
+		term_lists = [term_lists, term_list]
+
 	Scores = []
+	Length = [] # Holds numbers of docs for each term
+
+	for term in query_terms:
+		query_term_weight = calculate_query_term_weight(term,query)
+		term_list = get_dictionary_term_list(term)
+		for key in term_list:
+			Scores[key] += term_list[key]*query_term_weight
+			Length[key] += math.pow(Scores[key],2)
+
+	for i, score in enumerate(Scores):
+		Scores[i] = score / Length[i]
+
 	return Scores
