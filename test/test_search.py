2f592b9630ff3e045f2c22ab167af10c118bbab7
Moritz Schäfer <mail@moritzs.de>
2016-04-07 20:59:47 +0800

    boilerplate

--- test/test_search.py
+++ test/test_search.py
@@ -3,10 +3,9 @@ from mock import patch
 from tsg.search import search
 
 @patch('tsg.parser.parse_text')
-def test_search(parse_text_mock):
-    #search()
+@patch('tsg.ranker.parse_text')
+def test_search():
     # should call (query)parser
     # should call ranker
     # should return list
     # scores should be sorted
-    pass
--- tsg/search.py
+++ tsg/search.py
@@ -1,22 +1,3 @@
-from tsg.parser import parse_text
-from tsg.ranker import rank
 
 def search(searchphrase, index_directory):
-    """
-    Processes a search query and returns a list of matched documents.
-
-    searchphrase: An unformatted search query
-    index_directory: The directory where dictionary.dat and indexinfo.json lie.
-
-    IMPORTANT: Use the dictionary.dat path as index_directory parameter
-    until the dictionary hash logic is defined.
-    """
-
-    parsed_query = parse_text(searchphrase)
-
-    # TODO Modify get_dictionary_term_list in tsg.ranker.base to 
-    # use the hash dictionary and read a position in dictionary 
-    # instead of line by line
-
-    return rank(parsed_query, index_directory)
-
+    pass
