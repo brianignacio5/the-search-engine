df86160cb55179b80f4cc69420ab05ed11bb9248
Moritz Schäfer <mail@moritzs.de>
2016-04-08 02:36:38 +0800

    naive search function

--- tsg/search.py
+++ tsg/search.py
@@ -1,3 +1,11 @@
+from tsg.parser import parse_text
+from tsg.ranker import rank
+
+from config import DIRECTORY
 
 def search(searchphrase, index_directory):
-    pass
+
+    parsed_query = parse_text(searchphrase)
+
+    return rank(parsed_query, index_directory)
+
