4edf4c724c10a09a930ed0b6a7fc5da885995a12
Moritz Schäfer <mail@moritzs.de>
2016-04-09 01:19:10 +0800

    add description to search function

--- test/test_search.py
+++ test/test_search.py
@@ -3,9 +3,10 @@ from mock import patch
 from tsg.search import search
 
 @patch('tsg.parser.parse_text')
-@patch('tsg.ranker.parse_text')
-def test_search():
+def test_search(parse_text_mock):
+    #search()
     # should call (query)parser
     # should call ranker
     # should return list
     # scores should be sorted
+    pass
