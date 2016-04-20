3445dca6c59435d324495f6978c8f011a051becd
Brian Ignacio <brianignacio5@gmail.com>
2016-04-21 07:18:01 +0800

    Added AND search and tests

--- tsg/ranker/base.py
+++ tsg/ranker/base.py
@@ -47,6 +47,47 @@ def calculate_query_term_weight(term, query_terms, index_dictionary_path=DICTION
 
     return weight
 
+def conjunctive_score_calc(query_terms, index_dictionary_path= DICTIONARY_PATH, 
+    index_info_path = INDEXINFO_PATH):
+    
+    results_keys = set()
+    terms_lists = {}
+    scores = {}
+    doc_length = {}
+    query_length = {}
+
+    for term in query_terms:
+        terms_lists[term] = get_dictionary_term_list(term, index_dictionary_path)
+        if len(results_keys) == 0:
+            results_keys = terms_lists[term].keys()
+        else:
+            results_keys &= terms_lists[term].keys()
+
+    for key in results_keys:
+        for term in query_terms:
+            if key in terms_lists[term].keys():
+                query_term_weight = calculate_query_term_weight(term,query_terms,
+            index_dictionary_path, index_info_path)
+                if key in scores:
+                    scores[key] += float(terms_lists[term][key])*float(query_term_weight)
+                else:
+                    scores[key] = float(terms_lists[term][key])*float(query_term_weight)
+
+                if key in doc_length:
+                    doc_length[key] += math.pow(float(terms_lists[term][key]), float(2))
+                    query_length[key] += math.pow(float(query_term_weight), float(2))
+                else:
+                    doc_length[key] = math.pow(float(terms_lists[term][key]), float(2))
+                    query_length[key] = math.pow(float(query_term_weight), float(2))
+
+    for key in scores:
+        try:
+            scores[key] = scores[key] / (math.sqrt(doc_length[key])*math.sqrt(query_length[key]))
+        except ZeroDivisionError:
+            scores[key] = 0
+
+    return scores
+
 def cosine_score_calc(query_terms, index_dictionary_path=DICTIONARY_PATH,
     index_info_path = INDEXINFO_PATH):
 
@@ -64,17 +105,27 @@ def cosine_score_calc(query_terms, index_dictionary_path=DICTIONARY_PATH,
                 scores[key] = float(value)*float(query_term_weight)
 
             if key in query_length:
-                query_length[key] += math.pow(float(query_term_weight),float(2))
+                query_length[key] += math.pow(float(query_term_weight), float(2))
                 doc_length[key] += math.pow(float(value),float(2))
             else:
-                query_length[key] = math.pow(float(query_term_weight),float(2))
-                doc_length[key] = math.pow(float(value),float(2))
+                query_length[key] = math.pow(float(query_term_weight), float(2))
+                doc_length[key] = math.pow(float(value), float(2))
 
     for key in scores:
         try:
             scores[key] = scores[key] / (math.sqrt(query_length[key])*math.sqrt(doc_length[key]))
         except ZeroDivisionError:
             scores[key] = 0
+
+    return scores
+
+def combine_and_or_scores(and_dict, or_dict):
+    sorted_and = sorted(and_dict.items(), key = operator.itemgetter(1,0))
+    sorted_or = sorted(or_dict.items(), key = operator.itemgetter(1, 0))
+
+    new_docs = set(sorted_or) - set(sorted_and)
+
+    scores = sorted_and + list(new_docs)
 
     return scores
 
@@ -85,12 +136,12 @@ def rank(query_terms, index_dictionary_path=DICTIONARY_PATH,
     and retrieved a list of docs ordered by score and doc_id as
     tuples [(doc_1,score_1),(doc_2, score_2), ..., (doc_n,score_n)]
     '''
-    cosine_scores = cosine_score_calc(query_terms, index_dictionary_path, index_info_path)
+    and_scores = conjunctive_score_calc(query_terms, index_dictionary_path, index_info_path)
+    or_scores = cosine_score_calc(query_terms, index_dictionary_path, index_info_path)
 
     # TODO Add PageRank scores
     # loop: for each key (docID) add pagerank(docId) score
 
-    # Sort by score value then doc_id, in case of equal score docs.
-    sorted_docs = sorted(cosine_scores.items(), key = operator.itemgetter(1,0))
+    sorted_docs = combine_and_or_scores(and_scores, or_scores)
 
     return sorted_docs
