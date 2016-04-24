40d858aa24c81662f38665d4955e2a4cfafbe8cf
Brian Ignacio <brianignacio5@gmail.com>
2016-04-25 04:49:10 +0800

    Running pagerank with green tests

--- test/test_page_ranker.py
+++ test/test_page_ranker.py
@@ -1,8 +1,10 @@
 from nose import with_setup
 import os
+from lxml import etree
 from tsg.config import DATA_DIR
-from tsg.ranker.page_rank import parse_link, build_link_database
+from tsg.ranker.page_rank import parse_link, build_link_database, get_page_outlinks, get_doc_inlinks, calc_page_rank
 from tsg.crawler.downloader import get_site
+from tsg.crawler.base import crawl_site
 
 RAW_TEST_DIRECTORY = DATA_DIR + "/test/files/raw/"
 files = [RAW_TEST_DIRECTORY + "author_a_A:Almaaf_Bader_Ali.html",
@@ -85,23 +87,40 @@ def create_raw_directory():
 	 				 'author_a_Abroud:Alireza.html': ['journal_ijea_ijea5.html'], 
 	 				 'author_p_Plasmeijer:Rinus.html': ['conference_aaip_aaip2009.html']}
 	 				 
-	with open(TEST_LINK_DATABASE_PATH,'w') as link_f:
-		if os.path.exists(link_f):
-			pass
-		else:
+	if not os.path.exists(TEST_LINK_DATABASE_PATH):
+		with open(TEST_LINK_DATABASE_PATH,'w') as link_f:
 			for key, value in test_link_database.items():
 				link_str = ""
-			for link in value:
-				link_str += "," + link
-			link_f.write(key + " " + link_str[1:] + "\n")
+				for link in value:
+					link_str += "," + link
+				link_f.write(key + " " + link_str[1:] + "\n")
+
+	# for f in files:
+	# 	download_outlinks_by_filename(f)
+
+def download_outlinks_by_filename(file_path):
+	xpath_string = "//div[@class='data']//a/@href"
+	parser = etree.HTMLParser()
+	outlinks = []
+	
+	with open(file_path) as doc_f:
+		tree = etree.parse(doc_f, parser)
+		outlinks = tree.xpath(xpath_string)
+
+	for link in outlinks:
+		document_type = parse_link(link)[1]
+		crawl_site(link, document_type)
+
 
 def delete_raw_files():
 	remove_flag = False
 	try:
-		for f in files:
-			if remove_flag:
+		if remove_flag:
+			for f in files:
 				os.remove(f)
 				assert not os.path.exists(f)
+			os.remove(TEST_LINK_DATABASE_PATH)
+			assert not os.path.exists(TEST_LINK_DATABASE_PATH)
 	except OSError:
 		pass
 
@@ -114,18 +133,31 @@ def test_parse_link():
 
 	assert filename[0] == test_filename
 
+def test_get_page_outlinks():
+	test_filename = RAW_TEST_DIRECTORY + "author_a_A:Almaaf_Bader_Ali.html"
+
+	test_outlinks = ['author_m_Miao:Jian=Jun.html', 
+					 'author_t_Tran:Quang=Dung.html',
+					 'journal_ijea_ijea5.html']
+
+	outlinks_by_function = get_page_outlinks(test_filename)
+
+	assert outlinks_by_function == test_outlinks
+
 @with_setup(create_raw_directory, delete_raw_files)
-def test_get_docs_for_target_doc():
+def test_build_link_database():
 
 	#define some docs in DATA_DIR/test/files to use here.
 	test_doc_dict = {'author_w_Wilbon:Anthony_D=.html': ['journal_ijea_ijea5.html'],
 	 				 'author_h_Harris:Tom.html': ['journal_ijea_ijea5.html'], 
 	 				 'author_s_Smith:Sheila_M=.html': ['journal_ijea_ijea5.html'], 
 	 				 'author_s_Sharma:Sushil_Kumar.html': ['journal_ijea_ijea5.html'], 
-	 				 'author_r_Ram=iacute=rez=Quintana:M=_Jos=eacute=.html': ['conference_aaip_aaip2009.html'], 
+	 				 'author_r_Ram=iacute=rez=Quintana:M=_Jos=eacute=.html': 
+	 				 	['conference_aaip_aaip2009.html'], 
 	 				 'author_c_Chen:Yi=Fen.html': ['journal_ijea_ijea5.html'], 
 	 				 'author_h_Henderson:Robert.html': ['conference_aaip_aaip2009.html'], 
-	 				 'author_h_Hern=aacute=ndez=Orallo:Jos=eacute=.html': ['conference_aaip_aaip2009.html'], 
+	 				 'author_h_Hern=aacute=ndez=Orallo:Jos=eacute=.html': 
+	 				 	['conference_aaip_aaip2009.html'], 
 	 				 'author_z_Zhang:Changzheng.html': ['journal_ijea_ijea5.html'], 
 	 				 'author_s_Saprikis:Vaggelis.html': ['journal_ijea_ijea5.html'], 
 	 				 'author_e_Estruch:Vicent.html': ['conference_aaip_aaip2009.html'], 
@@ -139,23 +171,28 @@ def test_get_docs_for_target_doc():
 	 				 'author_t_Tran:Quangdung.html': ['journal_ijea_ijea5.html'], 
 	 				 'author_h_Huang:Dechun.html': ['journal_ijea_ijea5.html'], 
 	 				 'author_t_Tsai:Chia=Wen.html': ['journal_ijea_ijea5.html'], 
-	 				 'author_m_Miao:Jian=Jun.html': ['journal_ijea_ijea5.html', 'author_a_A:Almaaf_Bader_Ali.html', 'author_t_Tran:Quang=Dung.html'], 
+	 				 'author_m_Miao:Jian=Jun.html': ['journal_ijea_ijea5.html', 
+	 				 	'author_a_A:Almaaf_Bader_Ali.html', 'author_t_Tran:Quang=Dung.html'], 
 	 				 'author_k_Kitzelmann:Emanuel.html': ['conference_aaip_aaip2009.html'], 
 	 				 'author_h_Hsing:San=San.html': ['journal_ijea_ijea5.html'], 
 	 				 'author_s_Smith:Sheila.html': ['journal_ijea_ijea5.html'], 
 	 				 'author_d_Darisipudi:Ashok.html': ['journal_ijea_ijea5.html'],
-	 				 'author_a_A:Almaaf_Bader_Ali.html': ['journal_ijea_ijea5.html', 'author_t_Tran:Quang=Dung.html', 'author_m_Miao:Jian=Jun.html'], 
+	 				 'author_a_A:Almaaf_Bader_Ali.html': ['journal_ijea_ijea5.html', 
+	 				 	'author_t_Tran:Quang=Dung.html', 'author_m_Miao:Jian=Jun.html'], 
 	 				 'journal_journals_lncs.html': ['conference_aaip_aaip2009.html'], 
 	 				 'author_a_Alexander:Melody.html': ['journal_ijea_ijea5.html'], 
 	 				 'author_h_Hofmann_0008:Martin.html': ['conference_aaip_aaip2009.html'], 
-	 				 'author_t_Tran:Quang=Dung.html': ['journal_ijea_ijea5.html', 'author_a_A:Almaaf_Bader_Ali.html', 'author_m_Miao:Jian=Jun.html'], 
+	 				 'author_t_Tran:Quang=Dung.html': ['journal_ijea_ijea5.html', 
+	 				 	'author_a_A:Almaaf_Bader_Ali.html', 'author_m_Miao:Jian=Jun.html'], 
 	 				 'author_a_Abdelghaffar:Hany.html': ['journal_ijea_ijea5.html'], 
 	 				 'author_m_Muthaiyah:Saravanan.html': ['journal_ijea_ijea5.html'], 
 	 				 'author_z_Zhao:Jensen.html': ['journal_ijea_ijea5.html'], 
 	 				 'author_s_Sn=aacute=sel:V=aacute=clav.html': ['journal_ijea_ijea5.html'], 
-	 				 'author_y_Yakushev:Alexey_Rodriguez.html': ['conference_aaip_aaip2009.html'], 
+	 				 'author_y_Yakushev:Alexey_Rodriguez.html': 
+	 				 	['conference_aaip_aaip2009.html'], 
 	 				 'author_c_Coverdale:Tonjia_S=.html': ['journal_ijea_ijea5.html'], 
-	 				 'journal_ijea_ijea5.html': ['author_a_A:Almaaf_Bader_Ali.html', 'author_t_Tran:Quang=Dung.html', 'author_m_Miao:Jian=Jun.html'], 
+	 				 'journal_ijea_ijea5.html': ['author_a_A:Almaaf_Bader_Ali.html', 
+	 				 	'author_t_Tran:Quang=Dung.html', 'author_m_Miao:Jian=Jun.html'], 
 	 				 'author_h_Hsu:Huei=Hsia.html': ['journal_ijea_ijea5.html'], 
 	 				 'author_h_Hanafizadeh:Mohammad_Reza.html': ['journal_ijea_ijea5.html'], 
 	 				 'author_l_Lu:Hsi=Peng.html': ['journal_ijea_ijea5.html'], 
@@ -181,9 +218,71 @@ def test_get_docs_for_target_doc():
 	assert doc_dict == test_doc_dict
 
 @with_setup(create_raw_directory, delete_raw_files)
+def test_get_doc_inlinks():
+
+	filename = "author_a_A:Almaaf_Bader_Ali.html"
+
+	test_inlinks = ['journal_ijea_ijea5.html', 
+					'author_t_Tran:Quang=Dung.html', 
+					'author_m_Miao:Jian=Jun.html']
+	
+	doc_inlinks_by_function = get_doc_inlinks(filename, TEST_LINK_DATABASE_PATH)
+
+	assert doc_inlinks_by_function == test_inlinks
+
+@with_setup(create_raw_directory, delete_raw_files)
 def test_calc_page_rank():
 
-	# Is it testable ?
-	#
+	test_pagerank_per_doc = [('journal_ijea_ijea5.html', 1), 
+							 ('author_t_Tran:Quang=Dung.html', 0.7373983739837399), 
+							 ('author_m_Miao:Jian=Jun.html', 0.7373983739837399), 
+							 ('author_a_A:Almaaf_Bader_Ali.html', 0.7373983739837399), 
+							 ('author_z_Zhou:Tao.html', 0.17073170731707318), 
+							 ('author_z_Zhang:Jeff.html', 0.17073170731707318), 
+							 ('author_w_Wilbon:Anthony_D=.html', 0.17073170731707318), 
+							 ('author_t_Tsai:Chia=Wen.html', 0.17073170731707318), 
+							 ('author_t_Tran:Quangdung.html', 0.17073170731707318), 
+							 ('author_s_Sn=aacute=sel:V=aacute=clav.html', 0.17073170731707318), 
+							 ('author_s_Smith:Sheila_M=.html', 0.17073170731707318), 
+							 ('author_s_Smith:Sheila.html', 0.17073170731707318), 
+							 ('author_s_Shih:Tsui=Yii.html', 0.17073170731707318), 
+							 ('author_s_Sharma:Sushil_Kumar.html', 0.17073170731707318), 
+							 ('author_s_Saprikis:Vaggelis.html', 0.17073170731707318), 
+							 ('author_m_Muthaiyah:Saravanan.html', 0.17073170731707318), 
+							 ('author_m_Moustafa:Hussien.html', 0.17073170731707318), 
+							 ('author_l_Lu:Hsi=Peng.html', 0.17073170731707318), 
+							 ('author_l_Lin:Tzu=Hong.html', 0.17073170731707318), 
+							 ('author_l_Lin:Shu=Yu.html', 0.17073170731707318), 
+							 ('author_h_Huang:Dechun.html', 0.17073170731707318), 
+							 ('author_h_Hsu:Huei=Hsia.html', 0.17073170731707318), 
+							 ('author_h_Hsing:San=San.html', 0.17073170731707318), 
+							 ('author_h_Ho:Tai=Li.html', 0.17073170731707318), 
+							 ('author_h_Hanafizadeh:Payam.html', 0.17073170731707318), 
+							 ('author_h_Hanafizadeh:Mohammad_Reza.html', 0.17073170731707318), 
+							 ('author_d_Darisipudi:Ashok.html', 0.17073170731707318), 
+							 ('author_c_Coverdale:Tonjia_S=.html', 0.17073170731707318), 
+							 ('author_c_Choong:Yap_Voon.html', 0.17073170731707318), 
+							 ('author_c_Chen:Yi=Fen.html', 0.17073170731707318), 
+							 ('author_c_Chen:Bo=Ying.html', 0.17073170731707318), 
+							 ('author_b_Bohlin:Erik.html', 0.17073170731707318), 
+							 ('author_a_Alwahaishi:Saleh.html', 0.17073170731707318), 
+							 ('author_a_Alexander:Melody.html', 0.17073170731707318), 
+							 ('author_a_Al=Jenaibi:Badreya.html', 0.17073170731707318), 
+							 ('author_a_Abroud:Alireza.html', 0.17073170731707318), 
+							 ('author_a_Abdelghaffar:Hany.html', 0.17073170731707318), 
+							 ('journal_journals_lncs.html', 0.15000000000000002), 
+							 ('author_y_Yakushev:Alexey_Rodriguez.html', 0.15000000000000002), 
+							 ('author_s_Schmid:Ute.html', 0.15000000000000002), 
+							 ('author_r_Ram=iacute=rez=Quintana:M=_Jos=eacute=.html', 0.15000000000000002), 
+							 ('author_p_Plasmeijer:Rinus.html', 0.15000000000000002), 
+							 ('author_m_Mitchell:Neil.html', 0.15000000000000002), 
+							 ('author_k_Koopman:Pieter_W=_M=.html', 0.15000000000000002), 
+							 ('author_k_Katayama:Susumu.html', 0.15000000000000002), 
+							 ('author_j_Jeuring:Johan.html', 0.15000000000000002), 
+							 ('author_h_Hern=aacute=ndez=Orallo:Jos=eacute=.html', 0.15000000000000002), 
+							 ('author_h_Henderson:Robert.html', 0.15000000000000002), 
+							 ('author_f_Ferri:C=eacute=sar.html', 0.15000000000000002)]
 
-	assert True == True
+	pagerank_per_doc = calc_page_rank(TEST_LINK_DATABASE_PATH)
+
+	assert pagerank_per_doc == test_pagerank_per_doc
--- tsg/ranker/page_rank.py
+++ tsg/ranker/page_rank.py
@@ -1,7 +1,9 @@
 import re
+from collections import deque, OrderedDict
 from lxml import etree
 import glob, os
-from tsg.config import RAW_DIR
+from tsg.config import RAW_DIR, DATA_DIR
+import operator
 
 def parse_link(doc_link):
 	'''
@@ -30,20 +32,31 @@ def parse_link(doc_link):
 
 	return [doc_filename, category]
 
+def get_page_outlinks(doc_path):
+	xpath_string = "//div[@class='data']//a/@href"
+	parser = etree.HTMLParser()
+	page_outlinks = []
+	page_outfiles = []
+
+	with open(doc_path) as doc_f:
+		tree = etree.parse(doc_f, parser)
+		page_outlinks = tree.xpath(xpath_string)
+
+	for link in page_outlinks:
+		page_outfiles.append(parse_link(link)[0])
+
+	return page_outfiles
+
 def build_link_database(html_files_path= RAW_DIR):
 	doc_dict = {}
 	# xpath_string = "//div[@id='main']/ul[@class='publ-list']//span/a/@href"
-	xpath_string = "//div[@class='data']//a/@href" # OK for Journals and authors
-	parser = etree.HTMLParser()
 	os.chdir(html_files_path)
 	for doc_filename in glob.glob("*.html"):
 		doc_path = html_files_path + doc_filename
-		with open(doc_path, 'r') as doc:
-			tree = etree.parse(doc, parser)
-			target_doc_links = tree.xpath(xpath_string) #Get links doc_path
-			# Classify links to author, journal and conference.
-			for target_doc_link in target_doc_links:
-				[target_doc, doc_type] = parse_link(target_doc_link)
+		doc_outlinks = get_page_outlinks(doc_path)
+		for target_doc in doc_outlinks:
+				#[target_doc, doc_type] = parse_link(target_doc_link)
+				doc_type = target_doc.split('_')[0]
 				#Return doc_filename and doc_type: author, conference or journal
 				if doc_type in ["author", "conference", "journal"]:
 					if (target_doc in doc_dict and doc_filename not in doc_dict[target_doc]):
@@ -51,17 +64,74 @@ def build_link_database(html_files_path= RAW_DIR):
 						doc_dict[target_doc].append(doc_filename)
 					elif target_doc not in doc_dict:
 						doc_dict[target_doc] = [doc_filename]
+	# Sort alphabetically
+	ordered_db = OrderedDict(sorted(doc_dict.items(), key= lambda t: t[0])) 
 
-	return doc_dict
+	LINK_DATABASE_PATH = DATA_DIR + "link_database.dat" #Replace LINK_DATABASE_PATH in config
 
-def calc_page_rank(doc_filename, link_database_path):
+	if not os.path.exists(DATA_DIR): 
+		with open(LINK_DATABASE_PATH,'w') as link_f:
+			for key, value in ordered_db.items():
+				link_str = ""
+				for link in value:
+					link_str += "," + link
+				link_f.write(key + " " + link_str[1:] + "\n")
 
-	# Something nice
-	doc_inlinks= []
+	return ordered_db
 
+def get_doc_inlinks(doc_filename, link_database_path):
+	doc_inlinks = []
 	with open(link_database_path) as link_db:
 		for line in link_db:
 			if line.startswith(doc_filename):
-				doc_inlinks = list(line[len(doc_filename)+1:].split(','))
+				doc_inlinks = list(line[len(doc_filename)+1:].strip().split(','))
 
-	return 0
+	return doc_inlinks
+
+def calc_page_rank(link_database_path):
+
+	# Something nice
+	d = 0.85 # Damping in PageRank Algorithm
+	threshold = 0.0000001 # 1x 10^-6
+	iteration_flag = True # Keep page rank iteration until threshold is met
+
+	link_db_docs_queue = deque()
+	doc_inlinks = {}
+	doc_outlinks = {}
+	pagerank_per_doc = {}
+
+	with open(link_database_path) as link_db:
+		for line in link_db:
+			current_doc = line.split(' ')[0]
+			doc_inlinks[current_doc] = list(line[len(current_doc)+1:].strip().split(','))
+			doc_outlinks[current_doc] = get_page_outlinks(RAW_DIR + current_doc)
+			link_db_docs_queue.append(current_doc)
+			pagerank_per_doc[current_doc] = 1
+
+	while iteration_flag:
+		tmp_queue = link_db_docs_queue
+		tmp_pagerank_per_doc = {}
+
+		while len(tmp_queue) > 0:
+			#page rank pop of queue
+			current_doc_in_queue = tmp_queue.pop()
+			tmp_pagerank_per_doc[current_doc_in_queue] = (1 - d)
+			for inlink in doc_inlinks[current_doc_in_queue]:
+				num_outlinks_per_inlink = 0
+				if inlink in doc_outlinks.keys():
+					num_outlinks_per_inlink = len(doc_outlinks[inlink])
+					tmp_pagerank_per_doc[current_doc_in_queue] += d*(pagerank_per_doc[inlink] 
+						/ num_outlinks_per_inlink)
+				else:
+					pass
+
+		for doc in tmp_pagerank_per_doc:
+			if (pagerank_per_doc[doc] - tmp_pagerank_per_doc[doc] < threshold):
+				iteration_flag = False
+			else:
+				pagerank_per_doc[doc] = tmp_pagerank_per_doc[doc]
+
+	sorted_pagerank_per_docs = sorted(pagerank_per_doc.items(), 
+		key = operator.itemgetter(1,0), reverse = True)
+
+	return sorted_pagerank_per_docs
