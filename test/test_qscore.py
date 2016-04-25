5f6e18caa4ca465e676928c033870dbf25fa4d93
Moritz Schäfer <mail@moritzs.de>
2016-04-25 14:10:12 +0800

    use qscore in indexer

--- test/test_qscore.py
+++ test/test_qscore.py
@@ -1,11 +1,24 @@
-from tsg.intermediate import qscore
+from tsg.indexer import qscore
 from nose.tools import eq_
 
 
-def test_get_distributions():
+def test_get_scores():
     parsed_test_dir = 'test/files/parsed'
-    distributions = qscore.get_distributions(parsed_test_dir)
+    qscores = qscore.get_scores(parsed_test_dir)
 
-    eq_(distributions['conference'], (1.0, 0.0))
-    eq_(distributions['conference_overview'], (2.0 ,2.0))
-    eq_(distributions['author'], (3.0, 0.0))
+    eq_(qscores['first'], 2/3)
+    eq_(qscores['second'], 2/3)
+    eq_(qscores['third'], 0.71955175131048577)
+    eq_(qscores['fourth'], 0.94711491535618098)
+
+def test_normalized_quality_score():
+    mean = 2
+    std = 2
+    value = 6
+    eq_(qscore._normalized_quality_score(mean, std, value), 0.99241662268394026)
+
+    # std == 0
+    eq_(qscore._normalized_quality_score(1, 0, 2), 1)
+    eq_(qscore._normalized_quality_score(1, 0, 3), 1)
+    eq_(qscore._normalized_quality_score(1, 0, 0), 2/3)
+    eq_(qscore._normalized_quality_score(3, 0, 0), 2/3)
