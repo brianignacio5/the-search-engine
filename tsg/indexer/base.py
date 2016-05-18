32b0b063742b924a90ea72816729e6e420e18d7b
Moritz Schäfer <mail@moritzs.de>
2016-05-18 16:33:15 +0800

    scale pagerank in indexer

--- tsg/indexer/base.py
+++ tsg/indexer/base.py
@@ -24,6 +24,10 @@ def parse_term(term_file, N, qscores, pagerank_scores):
     df_pagerank_scores = term_df.apply(lambda row: pagerank_scores.loc[row.name].pagerank_score,
                                        axis=1)
 
+    # log page rank scores and move minimum to 1
+    df_pagerank_scores = np.log10(df_pagerank_scores)
+    df_pagerank_scores += 1 - np.min(df_pagerank_scores)
+
     term_df['tf-idf'] = \
         log_weights * \
         df_qscores * \
