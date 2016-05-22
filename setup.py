a136e4b0fbe1b3030048d567109826b76a1274c7
Moritz Schäfer <mail@moritzs.de>
2016-05-22 16:09:29 +0200

    add flask dependencies

--- setup.py
+++ setup.py
@@ -24,6 +24,7 @@ setup(
                       'mock',
                       'pandas',
                       'numpy',
+                      'flask',
                       'validators',
                       'scipy']
 )
